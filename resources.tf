d24fa9831cde7f4e047963720f6c0870809b8623
ayroid <ayushsk0000@gmail.com>
2024-06-29 15:39:08 +0530

    feat: project setup

---
+++ resources.tf
@@ -0,0 +1,1 @@
+# Resources to be created
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS Default Region"
+}
+
+variable "aws_access_key" {
+  type        = string
+  description = "AWS Admin Access Key"
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS Admin Secret Key"
+}
