c50a13fafdbd86fbf035c4c12c571e9a4fee971c
ayroid <ayushsk0000@gmail.com>
2024-06-30 23:22:35 +0530

    feat: used vpc module provided by terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,26 @@
-resource "aws_vpc" "my-vpc" {
-  cidr_block = var.vpc_cidr_block
+module "vpc" {
+  source         = "terraform-aws-modules/vpc/aws"
+  name           = var.vpc_name
+  cidr           = var.vpc_cidr_block
+  azs            = [var.availability_zone]
+  public_subnets = [var.subnet_cidr_block]
+  public_subnet_tags = {
+    Name = "${var.env_prefix}-${var.subnet_name}"
+  }
   tags = {
     Name = "${var.env_prefix}-${var.vpc_name}"
   }
 }
 
-module "subnet-module" {
-  source            = "./modules/subnet"
-  vpc_id            = aws_vpc.my-vpc.id
-  subnet_cidr_block = var.subnet_cidr_block
-  availability_zone = var.availability_zone
-  env_prefix        = var.env_prefix
-  subnet_name       = var.subnet_name
-}
-
 module "ec2server-module" {
   source         = "./modules/ec2server"
   env_prefix     = var.env_prefix
-  vpc_id         = aws_vpc.my-vpc.id
+  vpc_id         = module.vpc.vpc_id
   my_ip          = var.my_ip
   key_name       = var.key_name
   key_path       = var.key_path
   aws_image_name = var.aws_image_name
   instance_type  = var.instance_type
-  subnet_id      = module.subnet-module.subnet_id
+  subnet_id      = module.vpc.public_subnets[0]
 }
 
--- terraform/modules/subnet/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "subnet_id" {
-  value = aws_subnet.my-subnet-1.id
-}
